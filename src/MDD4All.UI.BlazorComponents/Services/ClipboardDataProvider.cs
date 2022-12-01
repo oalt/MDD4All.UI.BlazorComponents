9d55f790377305696c5c21ba730f8f3ded3f4ef2
Oliver Alt <oa@lacheweg21.de>
2022-12-01 22:21:35 +0100

    Add clipboard provider and some style changes

---
+++ src/MDD4All.UI.BlazorComponents/Services/ClipboardDataProvider.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.JSInterop;
+
+namespace MDD4All.UI.BlazorComponents.Services
+{
+    public sealed class ClipboardDataProvider
+    {
+        private readonly IJSRuntime _jsRuntime;
+
+        public ClipboardDataProvider(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public ValueTask<string> ReadTextAsync()
+        {
+            return _jsRuntime.InvokeAsync<string>("navigator.clipboard.readText");
+        }
+
+        public ValueTask WriteTextAsync(string text)
+        {
+            return _jsRuntime.InvokeVoidAsync("navigator.clipboard.writeText", text);
+        }
+    }
+}
