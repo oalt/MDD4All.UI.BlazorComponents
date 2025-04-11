d8feef9f600482025d27b51219b239aab52d76e0
Oliver Alt <oa@lacheweg21.de>
2025-04-11 17:41:24 +0200

    Adding Drag and Drop data provider

---
+++ src/MDD4All.UI.BlazorComponents/Services/DragDropDataProvider.cs
@@ -0,0 +1,18 @@
+﻿namespace MDD4All.UI.BlazorComponents.Services
+{
+    public class DragDropDataProvider
+    {
+        private object _data;
+
+        public void SetData(object data)
+        {
+            _data = data;
+        }
+        
+        public object GetData()
+        {
+            return _data;   
+        }
+    }
+
+}
